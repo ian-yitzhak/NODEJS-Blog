f46d4b7c7f8cbb9c69d3e0fd958cd9b949973832
Ian <onyangoian71@gmail.com>
2021-07-08 08:12:23 +0300

    Set up server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const blogRoute = require('./routes/blog')
+
+require('./db/db')
+app.use(express.json());
+app.set('views engine' , 'ejs')
+app.set('views', __dirname + 'views')
+
+app.get('/', (req,res)=>{
+	res.send('hello')
+})
+
+
+app.use('/blog', blogRoute)
+
+const port = 3000;
+app.listen(3000, ()=> console.log(`app running on ${port}`))
